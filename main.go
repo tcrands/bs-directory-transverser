f85955218c389ce545c21d0bcc0912859073912d
tcrands <tcrands@gmail.com>
2017-03-24 11:36:55 +0000

    working without file writing

--- main.go
+++ main.go
@@ -6,18 +6,58 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"io/ioutil"
+	"regexp"
 )
-
-func walkpath(path string, f os.FileInfo, err error) error {
-	// MODIFY THIS LINE TO PRINT OUT WHAT YOU WANT!
-	if strings.Contains(path, ".") {
-		fmt.Printf("<script type=\"text/brightscript\" uri=\"pkg:/source/sky/roku/client-lib-bs-ott/" + path + "\" />\n")
-	}
-	return nil
-}
 
 func main() {
 	flag.Parse()
 	root := flag.Arg(0) // 1st argument is the directory location
-	filepath.Walk(root, walkpath)
+	filepath.Walk(root, walkRootPath)
+}
+
+func walkRootPath(path string, f os.FileInfo, err error) error {
+	// MODIFY THIS LINE TO PRINT OUT WHAT YOU WANT!
+	if strings.Contains(path, ".xml") {
+		file, _ := ioutil.ReadFile(path)
+
+		subPath := getParams(`<!-- SCRIPT-INCLUDE uri="(?P<path>.*?)" -->`, string(file))
+
+		buffer := []string{}
+		filepath.Walk("./" + subPath["path"], walkSubPath(path, &buffer))
+		
+		writeToFile(buffer, file)
+
+	}
+	return nil
+}
+
+
+// REMEMBER! Good example of wrapping function for scoping
+func walkSubPath(origPath string, buffer *[]string) filepath.WalkFunc {
+	return func(path string, f os.FileInfo, err error) error {
+		if strings.Contains(path, ".brs") {
+			tag := "<script type=\"text/brightscript\" uri=\"pkg:/" + path + "\" />"
+			*buffer = append(*buffer, tag)
+		}
+		return nil
+	}
+}
+
+func writeToFile(buffer []string, file []byte) {
+	fmt.Println("WRITE BUFFER TO FILE")
+}
+
+func getParams(regEx, url string) (paramsMap map[string]string) {
+
+    compRegEx := regexp.MustCompile(regEx)
+    match := compRegEx.FindStringSubmatch(url)
+
+    paramsMap = make(map[string]string)
+    for i, name := range compRegEx.SubexpNames() {
+        if i > 0 && i <= len(match) {
+            paramsMap[name] = match[i]
+        }
+    }
+    return nil
 }
